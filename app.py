be9ca38aca66f2be60c600455c2c39c651805e63
yazaki-tatsuya <yazaki.tatsuya.0813@gmail.com>
2025-01-08 22:36:04 +0900

    改善対応：応答速度を向上するため、オーディオ再生キューを改善し、初期遅延を削減

--- app.py
+++ app.py
@@ -181,4 +181,6 @@ def tts():
 
 if __name__ == "__main__":
     # ローカルデバッグ用
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
